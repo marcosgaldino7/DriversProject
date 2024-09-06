960750ba4a07f70cb59df46a42e8d6971b6f77ee
marcos.galdino7 <marcos.galdino7@gmail.com>
2024-09-06 14:44:29 -0300

    update in files (app.js, index.html) for implementation of search element on the page

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 function pesquisar(){
     let section = document.getElementById("resultados-pesquisa")
 
-let resultados = ""
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-console.log(section)
+if (campoPesquisa == "" || !campoPesquisa.trim()){
+    section.innerHTML =` 
+    <div class="item-resultado">
+      <p class="descricao-meta">Nenhum piloto ou equipe encontrados.</p>
+    </div>`
+    
+    return
+   }
+
+   campoPesquisa = campoPesquisa.toLowerCase()
+
+let resultados=""; 
+let titulo="", descricao="";
 
 for (let dado of dados){ 
-resultados += `
+  titulo = dado.titulo.toLowerCase()
+  descricao = dado.descricao.toLowerCase()
+  if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
+    resultados += `
     <div class="item-resultado">
           <h2>${dado.titulo}</h2>
           <p class="descricao-meta">
@@ -18,8 +33,30 @@ resultados += `
           </a>
         </div>
 `
-}
+  } else  {
+    resultadosNegativos = `
+    <div class="item-resultado">
+          <p class="descricao-meta">
+            Nenhum piloto ou equipe encontrados com esse. Pesquise novamente
+          </p>
+        </div>
+`
 
+  }
+
+}
+if (!resultados){
+  resultados = `
+    <div class="item-resultado">
+          <p class="descricao-meta">
+            Nenhum piloto ou equipe encontrados com esse. Pesquise novamente
+          </p>
+        </div>
+`
+} 
+  
 section.innerHTML = resultados
+
+
 }
 
