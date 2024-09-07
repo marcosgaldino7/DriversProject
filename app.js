db65666a36b0f56695fd3837657e17a2688d4a0b
marcos.galdino7 <marcos.galdino7@gmail.com>
2024-09-07 15:32:17 -0300

    update to all files for better viewing on small devices

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// Função para realizar a pesquisa de pilotos e equipes
 function pesquisar(){
+    // Obtém a seção onde os resultados serão exibidos e o valor digitado no campo de pesquisa
     let section = document.getElementById("resultados-pesquisa")
 
     let campoPesquisa = document.getElementById("campo-pesquisa").value
 
+// Verifica se o campo de pesquisa está vazio. Se estiver, exibe uma mensagem informando que não foram encontrados resultados.
 if (campoPesquisa == "" || !campoPesquisa.trim()){
     section.innerHTML =` 
     <div class="item-resultado">
@@ -11,29 +14,52 @@ if (campoPesquisa == "" || !campoPesquisa.trim()){
     
     return
    }
-
+   // Converte a pesquisa para minúsculas para facilitar a comparação com os dados
    campoPesquisa = campoPesquisa.toLowerCase()
-
+// Inicializa variáveis para armazenar os resultados da pesquisa
 let resultados=""; 
-let titulo="", descricao="";
+let titulo="", descricao=""; tag="";
 
+// Itera sobre os dados, comparando a pesquisa com o título, descrição e tag de cada item
 for (let dado of dados){ 
   titulo = dado.titulo.toLowerCase()
   descricao = dado.descricao.toLowerCase()
-  if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa)){
-    resultados += `
-    <div class="item-resultado">
+  tag = dado.tag.toLowerCase()
+  
+  if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tag.includes(campoPesquisa)) {
+    // Verifica se é um piloto
+    if (dado.tag === 'Pilotos') { 
+        resultados += `
+            <<div class="item-resultado">
           <h2>${dado.titulo}</h2>
           <p class="descricao-meta">
-            ${dado.descricao}
+          Número de corrida : ${dado.numero}<br>  
+          ${dado.descricao}
           </p>
           <a
             href=${dado.link}
             target="_blank"> Mais Informações
           </a>
         </div>
-`
-  } else  {
+        `;
+      // Verifica se é uma equipe  
+    } else if (dado.tag === 'Equipes') { 
+        resultados += `
+            <div class="item-resultado equipe">
+                <h2>${dado.titulo}</h2>
+                <p class="descricao-meta">
+                     ${dado.descricao} <br>
+                     Sede: ${dado.cidadeSede} <br>
+                     País: ${dado.pais} <br>
+                     Títulos: ${dado.titulosConstrutores}<br>
+                     Piloto: <a href="#" onclick="pesquisarPiloto('${dado.pilotos1}')">${dado.pilotos1}</a> <br>
+                     Piloto: <a href="#" onclick="pesquisarPiloto('${dado.pilotos2}')">${dado.pilotos2}</a> 
+                </p>
+                <a href=${dado.link} target="_blank"> Mais Informações </a>
+            </div>
+        `;
+    }
+} else  {
     resultadosNegativos = `
     <div class="item-resultado">
           <p class="descricao-meta">
@@ -45,6 +71,7 @@ for (let dado of dados){
   }
 
 }
+// Verifica se foram encontrados resultados e atualiza a seção com os resultados ou uma mensagem de "nenhum resultado encontrado"
 if (!resultados){
   resultados = `
     <div class="item-resultado">
@@ -53,10 +80,41 @@ if (!resultados){
           </p>
         </div>
 `
-} 
+}
+ 
   
 section.innerHTML = resultados
 
 
+}
+//No resultado de equipes gera uma nova pesquisa para o piloto daquela equipe
+function pesquisarPiloto(nomePiloto) {
+  // Limpa os resultados anteriores
+  let section = document.getElementById("resultados-pesquisa");
+  section.innerHTML = "";
+
+  // Filtra os dados para encontrar o piloto
+  let piloto = dados.find(dado => dado.titulo.toLowerCase() === nomePiloto.toLowerCase());
+
+  if (piloto) {
+      // Exibe as informações do piloto
+      section.innerHTML = `
+          <div class="item-resultado">
+              <h2>${piloto.titulo}</h2>
+              <p class="descricao-meta">
+                  Número de corrida: ${piloto.numero}<br>
+                  ${piloto.descricao}
+              </p>
+              <a href=${piloto.link} target="_blank"> Mais Informações </a>
+          </div>
+      `;
+  } else {
+      // Exibe uma mensagem de erro
+      section.innerHTML = `
+          <div class="item-resultado">
+              <p class="descricao-meta">Nenhum piloto encontrado.</p>
+          </div>
+      `;
+  }
 }
 
--- dados.js
+++ dados.js
@@ -5,6 +5,7 @@ let dados = [
         numero: 1,
         equipe: "Red Bull Racing-Honda RBPT",
         link: "https://pt.wikipedia.org/wiki/Max_Verstappen",
+        tag: "Pilotos"
     },
     {
         titulo: "Sérgio Pérez",
@@ -12,6 +13,7 @@ let dados = [
         numero: 11,
         equipe: "Red Bull Racing-Honda RBPT",
         link: "https://pt.wikipedia.org/wiki/Sergio_Perez", 
+        tag: "Pilotos"
         
       },
       {
@@ -20,6 +22,7 @@ let dados = [
         numero: 44,
         equipe: "Mercedes-AMG Petronas Formula One Team",
         link: "https://pt.wikipedia.org/wiki/Lewis_Hamilton",
+        tag: "Pilotos"
       },
       {
         titulo: "George Russell",
@@ -27,70 +30,80 @@ let dados = [
         numero: 63,
         equipe: "Mercedes-AMG Petronas Formula One Team",
         link: "https://pt.wikipedia.org/wiki/George_Russell",
+        tag: "Pilotos"
       },
       {
         titulo: "Lando Norris",
-        descricao: "Jovem piloto britânico com grande potencial. Destacou-se desde as categorias de base e é um dos principais nomes da McLaren.",
+        descricao: "Jovem piloto britânico com grande potencial. Destacou-se desde as categorias de base e é um dos principais nomes da Formula 1 e considerando por muitos um futuro campeão.",
         numero: 4,
         equipe: "McLaren Formula 1 Team",
-        link: "https://pt.wikipedia.org/wiki/Lando_Norris"
+        link: "https://pt.wikipedia.org/wiki/Lando_Norris",
+        tag: "Pilotos"
       },
       {
         titulo: "Oscar Piastri",
         descricao: "Jovem piloto australiano, campeão da Fórmula 2 em 2021. Considerado uma das maiores promessas do automobilismo.",
         numero: 81,
         equipe: "McLaren Formula 1 Team",
-        link: "https://pt.wikipedia.org/wiki/Oscar_Piastri"
+        link: "https://pt.wikipedia.org/wiki/Oscar_Piastri",
+        tag: "Pilotos"
       },
       {
         titulo: "Charles Leclerc",
         descricao: "Piloto monegasco com grande talento e velocidade. Considerado uma das maiores promessas da Fórmula 1, já conquistou diversas pole positions e vitórias.",
         numero: 16,
         equipe: "Scuderia Ferrari HP",
-        link: "https://pt.wikipedia.org/wiki/Charles_Leclerc"
+        link: "https://pt.wikipedia.org/wiki/Charles_Leclerc",
+        tag: "Pilotos"
       },
       {
         titulo: "Carlos Sainz Jr",
         descricao: "Piloto espanhol conhecido por sua consistência e experiência. Foi campeão da Fórmula Renault 3.5 e tem sido uma peça importante da Ferrari.",
         numero: 55,
         equipe: "Scuderia Ferrari HP",
-        link: "https://pt.wikipedia.org/wiki/Carlos_Sainz_Jr."
+        link: "https://pt.wikipedia.org/wiki/Carlos_Sainz_Jr.",
+        tag: "Pilotos"
       },
       {
         titulo: "Fernando Alonso",
         descricao: "Piloto espanhol Bicampeão Mundial de Formula 1 com experiência em outras categorias, conhecido por sua habilidade e paixão. Considerado por muitos tecnicamente um dos melhores pilotos de todos os tempos, porém com vários problemas de relacionamento nas equipes que passou prejudicou sua vitoriosa carreira",
         numero: 14,
         equipe: "Aston Martin Aramco Cognizant Formula One Team",
-        link: "https://pt.wikipedia.org/wiki/Fernando_Alonso"
+        link: "https://pt.wikipedia.org/wiki/Fernando_Alonso",
+        tag: "Pilotos"
       },
       {
         titulo: "Lance Stroll",
-        descricao: "Piloto canadense que iniciou sua carreira na Fórmula 1 em idade jovem. Filho do dono da equipe Aston Martin, busca se consolidar como um piloto competitivo.",
+        descricao: "Piloto canadense que iniciou sua carreira na Fórmula 1 em idade jovem. Filho do dono da equipe, busca se consolidar como um piloto competitivo.",
         numero: 18,
         equipe: "Aston Martin Aramco Cognizant Formula One Team",
-        link: "https://pt.wikipedia.org/wiki/Lance_Stroll"
+        link: "https://pt.wikipedia.org/wiki/Lance_Stroll",
+        tag: "Pilotos"
       },
       {
         titulo: "Esteban Ocon",
         descricao: "Piloto francês conhecido por sua habilidade e consistência. Teve uma trajetória promissora nas categorias de base e conquistou sua primeira vitória na Fórmula 1 em 2021.",
         numero: 31,
         equipe: "BWT Alpine F1 Team",
-        link: "https://pt.wikipedia.org/wiki/Esteban_Ocon"
+        link: "https://pt.wikipedia.org/wiki/Esteban_Ocon",
+        tag: "Pilotos"
       },
       {
         titulo: "Pierre Gasly",
         descricao: "Piloto francês com grande talento e experiência na Fórmula 1. Conquistou uma vitória na Fórmula 1 e é conhecido por sua habilidade em ultrapassagens.",
         numero: 10,
         equipe: "BWT Alpine F1 Team",
-        link: "https://pt.wikipedia.org/wiki/Pierre_Gasly"
+        link: "https://pt.wikipedia.org/wiki/Pierre_Gasly",
+        tag: "Pilotos"
       },
 
     {
         titulo: "Alex Albon",
-        descricao: "Piloto tailandês com experiência na Fórmula 1. Conhecido por sua velocidade e habilidade em ultrapassagens, Albon já competiu por equipes como Red Bull e Williams.",
+        descricao: "Piloto tailandês com experiência na Fórmula 1. Conhecido por sua velocidade e habilidade em ultrapassagens. Albon tem feito ótima passagem por sua atual equipe e se destacando muito",
         numero: 23,
         equipe: "Williams Racing",
         link: "https://pt.wikipedia.org/wiki/Alex_Albon",
+        tag: "Pilotos"
     
   },
     {
@@ -99,6 +112,7 @@ let dados = [
         numero: 29, 
         equipe: "Williams Racing", 
         link: "https://pt.wikipedia.org/wiki/Franco_Colapinto",
+        tag: "Pilotos"
   
     },
     {
@@ -106,42 +120,48 @@ let dados = [
         descricao: "Piloto australiano conhecido por sua personalidade carismática e estilo de pilotagem agressivo. Venceu diversas corridas na Fórmula 1 e conquistou um título de Fórmula 2.",
         numero: 3,
         equipe: "Visa Cash App RB F1 Team",
-        link: "https://pt.wikipedia.org/wiki/Daniel_Ricciardo"
+        link: "https://pt.wikipedia.org/wiki/Daniel_Ricciardo",
+        tag: "Pilotos"
       },
       {
         titulo: "Yuki Tsunoda",
         descricao: "Jovem piloto japonês com grande potencial. Conhecido por sua velocidade e determinação, Tsunoda é uma das promessas da Fórmula 1.",
         numero: 22,
         equipe: "Visa Cash App RB F1 Team",
-        link: "https://pt.wikipedia.org/wiki/Yuki_Tsunoda"
+        link: "https://pt.wikipedia.org/wiki/Yuki_Tsunoda",
+        tag: "Pilotos"
       },
       {
         titulo: "Valtteri Bottas",
         descricao: "Piloto finlandês com vasta experiência na Fórmula 1. Conhecido por sua consistência e habilidade em qualificação, Bottas conquistou diversas vitórias e pódios.",
         numero: 77,
         equipe: "Stake F1 Team Kick Sauber",
-        link: "https://pt.wikipedia.org/wiki/Valtteri_Bottas"
+        link: "https://pt.wikipedia.org/wiki/Valtteri_Bottas",
+        tag: "Pilotos"
       },
       {
         titulo: "Guanyu Zhou",
         descricao: "Primeiro piloto chinês na Fórmula 1. Conhecido por sua coragem e determinação, Zhou busca se consolidar na categoria.",
         numero: 24,
         equipe: "Stake F1 Team Kick Sauber",
-        link: "https://pt.wikipedia.org/wiki/Guanyu_Zhou"
+        link: "https://pt.wikipedia.org/wiki/Guanyu_Zhou",
+        tag: "Pilotos"
       },
       {
         titulo: "Nico Hülkenberg",
         descricao: "Piloto alemão com vasta experiência na Fórmula 1. Conhecido por sua habilidade em ultrapassagens, Hülkenberg é um dos pilotos mais experientes do grid.",
         numero: 27,
         equipe: "MoneyGram Haas F1 Team[",
-        link: "https://pt.wikipedia.org/wiki/Nico_Hülkenberg"
+        link: "https://pt.wikipedia.org/wiki/Nico_Hülkenberg",
+        tag: "Pilotos"
       },
       {
         titulo: "Kevin Magnussen",
         descricao: "Piloto dinamarquês com experiência na Fórmula 1. Conhecido por sua agressividade e determinação, Magnussen já conquistou pódios na categoria.",
         numero: 20,
         equipe: "MoneyGram Haas F1 Team",
-        link: "https://pt.wikipedia.org/wiki/Kevin_Magnussen"
+        link: "https://pt.wikipedia.org/wiki/Kevin_Magnussen",
+        tag: "Pilotos"
       },
       {
         titulo: "Red Bull Racing-Honda RBPT",
@@ -152,16 +172,20 @@ let dados = [
         titulosPilotos: 5,
         pilotos1: "Max Verstappen",
         pilotos2: "Sérgio Pérez",
+        link: "https://pt.wikipedia.org/wiki/Red_Bull_Racing",
+        tag: "Equipes"
       },
       {
         titulo: "Mercedes-AMG Petronas Formula One Team",
         pais: "Alemanha",
         cidadeSede: "Brackley - Ingalaterra",
-        descricao: "Equipe de Fórmula 1 conhecida por sua dominância na década de 2010. Conquistou na década passada 7 titulos de pilotos 6 com Lewis Hamilton e um com o ex-piloto Nico Rosberg, tem histórico de construir motores de sucesso para a categoria.",
+        descricao: "Equipe de Fórmula 1 conhecida por sua dominância na década de 2010. Conquistou na década passada. tem histórico de construir motores de sucesso para a categoria. A equipe também é conhecida pelo apelido 'Flechas de Prata'",
         titulosConstrutores: 8,
         titulosPilotos: 7,
         pilotos1: "Lewis Hamilton",
-        pilotos2: "George Russell"
+        pilotos2: "George Russell",
+        link: "https://pt.wikipedia.org/wiki/Mercedes-Benz_na_Fórmula_1",
+        tag: "Equipes"
     },
     {
         titulo: "Scuderia Ferrari HP",
@@ -171,77 +195,93 @@ let dados = [
         titulosConstrutores: 16,
         titulosPilotos: 15,
         pilotos1: "Charles Leclerc",
-        pilotos2: "Carlos Sainz Jr"
+        pilotos2: "Carlos Sainz Jr",
+        link: "https://pt.wikipedia.org/wiki/Scuderia_Ferrari",
+        tag: "Equipes"
     }, 
     {
         titulo: "McLaren Formula 1 Team",
         pais: "Reino Unido",
         cidadeSede: "Woking - Inglaterra",
-        descricao: "Uma das equipes mais históricas da Fórmula 1, com diversos títulos mundiais.",
+        descricao: "Uma das equipes mais históricas da Fórmula 1, com diversos títulos mundiais. Marcou uma forte relação com o Brasil onde o saudoso e heroí Brasileiro Ayrton Senna conquistou seus 3 títulos mundiais",
         titulosConstrutores: 8,
         titulosPilotos: 12,
         pilotos1: "Lando Norris",
-        pilotos2: "Oscar Piastri"
+        pilotos2: "Oscar Piastri",
+        link: "https://pt.wikipedia.org/wiki/McLaren",
+        tag: "Equipes"
     },
     {
         titulo: "Aston Martin Aramco Cognizant Formula One Team",
         pais: "Reino Unido",
         cidadeSede: "Silverstone - Inglaterra",
-        descricao: "Equipe com uma longa história na Fórmula 1, atualmente com um projeto ambicioso para voltar ao topo.",
+        descricao: "Equipe com uma longa história na Fórmula 1, atualmente com um projeto ambicioso para voltar ao topo. Aston Martin conta com o pesado investimento financeiro do bilionário Laurence Stroll",
         titulosConstrutores: 2,
         titulosPilotos: 2,
         pilotos1: "Fernando Alonso",
-        pilotos2: "Lance Stroll"
+        pilotos2: "Lance Stroll",
+        link: "https://pt.wikipedia.org/wiki/Aston_Martin_na_Fórmula_1",
+        tag: "Equipes"
     },
     {
         titulo: "BWT Alpine F1 Team",
         pais: "França",
         cidadeSede: "Enstone - Inglaterra",
-        descricao: "Equipe francesa com raízes na Renault, buscando consolidar sua posição no grid.",
+        descricao: "Equipe francesa com raízes na Renault, a equipe Alpine tenta nos últimos anos se reinventar como equipe dentro da maior categoria de esporte a motor",
         titulosConstrutores: 2,
         titulosPilotos: 2,
         pilotos1: "Pierre Gasly",
-        pilotos2: "Esteban Ocon"
+        pilotos2: "Esteban Ocon",
+        link: "https://pt.wikipedia.org/wiki/Alpine_F1_Team",
+        tag: "Equipes"
     },
     {
         titulo: "Williams Racing",
         pais: "Reino Unido",
         cidadeSede: "Grove - Inglaterra",
-        descricao: "Uma das equipes mais tradicionais da Fórmula 1, buscando recuperar o desempenho dos anos dourados.",
+        descricao: "Uma das equipes mais tradicionais da Fórmula 1 com passagem de diversos pilotos incônicos, buscando recuperar o desempenho dos anos dourados.",
         titulosConstrutores: 9,
         titulosPilotos: 7,
         pilotos1: "Alex Albon",
-        pilotos2: "Franco Colapinto"
+        pilotos2: "Franco Colapinto",
+        link: "https://pt.wikipedia.org/wiki/Williams_Grand_Prix_Engineering",
+        tag: "Equipes"
     },
     {
         titulo: "Visa Cash App RB F1 Team",
         pais: "Itália",
         cidadeSede: "Faenza - Itália",
-        descricao: "Equipe irmã da Red Bull, com foco em jovens talentos.",
+        descricao: "Equipe irmã da RBR, tem com foco formar jovens para sua enorme academia de pilotos, atualmente muitos preferem chamar por seus antigos nomes 'Alhpa Thauri' ou 'Toro Rosso'.",
         titulosConstrutores: 0,
         titulosPilotos: 0,
         pilotos1: "Yuki Tsunoda",
-        pilotos2: "Daniel Ricciardo"
+        pilotos2: "Daniel Ricciardo",
+        link: "https://pt.wikipedia.org/wiki/RB_F1_Team",
+        tag: "Equipes"
     },
     {
         titulo: "Stake F1 Team Kick Sauber",
         pais: "Suíça",
         cidadeSede: "Hinwil - Suíça",
-        descricao: "Equipe suíça com um histórico de desenvolvimento de jovens talentos.",
+        descricao: "Equipe suíça com um histórico de desenvolvimento de jovens talentos. A equipe foi adquirida pela Audi que vai iniciar a operação na temporada 2026",
         titulosConstrutores: 0,
         titulosPilotos: 0,
         pilotos1: "Valtteri Bottas",
-        pilotos2: "Guanyu Zhou"
+        pilotos2: "Guanyu Zhou",
+        link: "https://pt.wikipedia.org/wiki/Sauber_Motorsport",
+        tag: "Equipes"
     },
     {
         titulo: "MoneyGram Haas F1 Team",
         pais: "Estados Unidos",
         cidadeSede: "Kannapolis - Estados Unidos",
-        descricao: "Equipe americana que entrou na Fórmula 1 em 2016.",
+        descricao: "Equipe americana do proprietário Gene Haas que participa de diversas competições de esporte a motor é a equipe mais jovem da categoria",
         titulosConstrutores: 0,
         titulosPilotos: 0,
         pilotos1: "Nico Hülkenberg",
-        pilotos2: "Kevin Magnussen"
+        pilotos2: "Kevin Magnussen",
+        link: "https://pt.wikipedia.org/wiki/Haas_F1_Team",
+        tag: "Equipes"
     }
 
 
